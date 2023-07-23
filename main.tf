8f068fb578eab9ec4c43ae69d6620482ee057b2f
MIHINDOU <gervaisolsen33@gmail.com>
2023-07-24 00:27:32 +0200

    request_mapping

--- main.tf
+++ main.tf
@@ -26,9 +26,7 @@ resource "aws_api_gateway_integration" "integration_request" {
   request_templates = {
     "application/json" = tostring({
       "headers" : {
-        #foreach($param in $input.params().header.keySet())
-        "$param" : "$util.escapeJavaScript($input.params().header.get($param))" #if($foreach.hasNext),#end
-        #end
+        "$param" : "$util.escapeJavaScript($input.params().header.get($param))"
       }
     })
   }
